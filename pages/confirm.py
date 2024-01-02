d539843198187dcc2b16fe24cd86e97620805553
HonaNguyen <87415934+HonaNguyen@users.noreply.github.com>
2024-01-02 15:27:18 +0700

    add confirm button and navigate to payment page

--- pages/confirm.py
+++ pages/confirm.py
@@ -3,12 +3,21 @@ from streamlit_extras.switch_page_button import switch_page
 
 
 def main():
+    # Cancel cart and navigate to menu
     cancel_cart()
+
+    # Confirm cart and go to payment page
+    payment_cart()
 
 
 def cancel_cart():
     if st.button("CANCEL"):
         switch_page("app")
+
+
+def payment_cart():
+    if st.button("CONFIRM"):
+        switch_page("payment_page")
 
 
 if __name__ == "__main__":
