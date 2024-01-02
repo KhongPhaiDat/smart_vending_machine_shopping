8714ade77c090320fad289b1a990ffc68540b250
HonaNguyen <87415934+HonaNguyen@users.noreply.github.com>
2024-01-02 15:20:46 +0700

    add cancel and redirect to menu page

--- pages/confirm.py
+++ pages/confirm.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
 
-st.write("AWS")
+def main():
+    cancel_cart()
+
+
+def cancel_cart():
+    if st.button("CANCEL"):
+        switch_page("app")
+
+
+if __name__ == "__main__":
+    main()
