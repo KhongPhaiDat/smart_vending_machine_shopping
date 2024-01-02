2bb4e45a45aa41e498c6f21be733bbe4d2304738
HonaNguyen <87415934+HonaNguyen@users.noreply.github.com>
2024-01-02 10:00:31 +0700

    finish total price

--- pages/menu.py
+++ pages/menu.py
@@ -3,6 +3,9 @@ import streamlit as st
 # Get machine info base on ID
 machine_info = dict()
 
+# Get item new quantity
+item_new_quantity = dict()
+
 # Just for testing to display
 machine_info = {
     "ID": "123456",
@@ -20,7 +23,14 @@ machine_info = {
 def main():
     st.title("Smart Vending Shopping")
 
+    for item in machine_info["items"].keys():
+        item_new_quantity[item] = machine_info["items"][item]["amount"]
+
+    # User input new quantity
     insert_amount_item()
+
+    # User can view total price
+    show_total_price()
 
 
 # Show info of item
@@ -30,8 +40,13 @@ def show_items(name_item, info_item):
 
 def get_new_quantity(name_item):
     new_quantity = st.number_input(
-        "Input your quantity: ", key=name_item, min_value=0, step=1
+        "Input your quantity: ",
+        key=name_item,
+        min_value=0,
+        max_value=machine_info["items"][name_item]["amount"],
+        step=1,
     )
+    return new_quantity
 
 
 # User can input amount to buy
@@ -40,12 +55,21 @@ def insert_amount_item():
 
     items = machine_info["items"]
 
-    num_items = len(items.keys())
-    item_new_quantity = dict()
-
     for item, info_item in items.items():
         show_items(item, info_item)
         item_new_quantity[item] = get_new_quantity(item)
+
+
+# Show total price of shopping
+def show_total_price():
+    total_price = 0
+
+    for item, new_quantity in item_new_quantity.items():
+        total_price = total_price + int(new_quantity) * int(
+            machine_info["items"][item]["price"]
+        )
+
+    st.write("Total Price: ", total_price)
 
 
 if __name__ == "__main__":
