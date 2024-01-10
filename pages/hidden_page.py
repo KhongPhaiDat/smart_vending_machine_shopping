c7d210861485d5cc9b858b8594ec06aeaed5157e
KhongPhaiDat <115723472+KhongPhaiDat@users.noreply.github.com>
2024-01-10 09:05:42 +0700

    Revert "save machine_id to session_state"

--- pages/hidden_page.py
+++ pages/hidden_page.py
@@ -2,12 +2,12 @@ import streamlit as st
 import modules.access_control as access_control
 
 
-if "session_id" not in st.session_state or "machine_id" not in st.session_state:
+if "session_id" not in st.session_state:
     st.write(
         "Bạn đang truy cập vào trang này bằng một cách không hợp lệ. Vui lòng quét mã QR và thử lại!!!"
     )
 else:
-    machine_id = st.session_state["machine_id"]
+    machine_id = st.experimental_get_query_params()["id"][0]
 
     access_control = access_control.AccessControl(
         machine_id=machine_id, session_id=st.session_state["session_id"]
